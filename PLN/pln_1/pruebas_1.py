07074000ff93062fbf75e573c167c15aabb2b7f5
FraustoEmi <e.fraustoortiz18@gmail.com>
2025-08-26 21:08:08 -0600

    Pruebas en PLN
    Descubrir la libreria de nltk

---
+++ PLN/pln_1/pruebas_1.py
@@ -0,0 +1,19 @@
+import nltk
+#nltk.download()
+
+from nltk.book import *
+'''
+print(text1)
+
+print(text1[-20:])
+
+print(nltk.data.path)
+
+print(len(set(text3))) #palabras unicas
+'''
+
+
+'''print(text4[-20:])'''
+
+print(type(text4))
+text4.dispersion_plot(["citizens"])
