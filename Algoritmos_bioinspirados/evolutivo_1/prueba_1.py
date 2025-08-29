c0b1ba8f44d5ddc3a99982f481410f3e6187dcee
FraustoEmi <e.fraustoortiz18@gmail.com>
2025-08-29 09:20:25 -0600

    Algoritmo genetico estocastico completo
    Se añadieron las funciones para mutar, seleccion y se implemento todas las funciones

--- Algoritmos_bioinspirados/evolutivo_1/prueba_1.py
+++ Algoritmos_bioinspirados/evolutivo_1/prueba_1.py
@@ -5,21 +5,14 @@ import matplotlib.pyplot as plt
 def f(x):
     return -(0.1+((1-x)**2)-0.1*np.cos(6*np.pi*(1-x))) + 2
 
-#Graficar la funcion
-x = np.linspace(0,2,100)
-y=f(x)
-plt.plot(x,y)
-plt.grid(True)
-#plt.show()
-
 #Creacion de individuos o poblacion
 def poblar(n,long): 
     '''n =      numero de individuos o tamaño de la poblacion''' 
     '''long =   la longitud de los cromosomas'''
     return np.random.randint(0,2,size=(n,long))
 
-p = poblar(10,8)
-print(p)
+'''p = poblar(10,8)
+print(p)'''
 
 #Funcion para decodificar los individuos de binario a decimal
 def decodificar(poblacion,Imin,Imax):
@@ -39,9 +32,9 @@ def decodificar(poblacion,Imin,Imax):
     
     return decimal,decimal_reescalado
 
-p_decodificado_entero , p_decodificado_decimal = decodificar(p,0,2)
+'''p_decodificado_entero , p_decodificado_decimal = decodificar(p,0,2)
 print(p_decodificado_entero)
-print(p_decodificado_decimal)
+print(p_decodificado_decimal)'''
 
 #Funcion para evualar los individuos y asi obtener las abtitudes (Fitness)
 def evaluar(poblacion,Imin,Imax):
@@ -50,14 +43,14 @@ def evaluar(poblacion,Imin,Imax):
     fitness = -(0.1+((1-individuo_decimal)**2)-0.1*np.cos(6*np.pi*(1-individuo_decimal))) + 2
     return fitness
 
-print(evaluar(p,0,2))
+'''print(evaluar(p,0,2))'''
 
 #Funcion que realiza la cruza de individuos
 def cruza(poblacion,pc):
     '''pc =     pares de cromosomas a cruzar'''
     r,c = poblacion.shape
-    M = np.zeros((2*pc,c))    #Matriz auxiliar para guardar decendientes
-    for par in range(pc):
+    M = np.zeros((int(2*pc),c))    #Matriz auxiliar para guardar decendientes
+    for par in range(int(pc)):
         r1 = np.random.randint(0,r,size=(1,2))  #par de padres 
         r2 = np.random.randint(0,c)
         
@@ -75,5 +68,85 @@ def cruza(poblacion,pc):
         
     return M
     
-cruzas = cruza(p,5)
-print(cruzas)
+'''cruzas = cruza(p,5)
+print(cruzas)'''
+
+def mutacion(poblacion, pm):
+    '''
+    pm = porcentaje de la poblacion a mutar
+    '''
+    r , c = poblacion.shape
+    n = int(pm*c)
+    for i in range(n):
+        r1 = np.random.randint(0,r) #numero aleatorio para seleccionar al individuo
+        r2 = np.random.randint(0,c) #numero aleatorio para seleccionar el gen a mutar
+        if (poblacion[r1,r2] == 0):
+            poblacion[r1,r2] = 1
+        else:
+            poblacion[r1,r2] = 0
+    return poblacion
+
+#FUNCION QUE REALIZA EL PROCESO DE SELECCION POR RANKING
+def hacer_ranking(poblacion,ps,Imin,Imax):
+    '''
+    ps = porcentaje de poblacion para seleccionar
+    '''
+    r,c = poblacion.shape
+    next_gen = np.zeros([r,c])
+    n = int(ps*r)
+    aptitud = evaluar(poblacion,Imin,Imax).reshape(r,1)
+    poblacion_ampliada = np.concatenate([poblacion,aptitud],axis=1)
+    indices = np.argsort(poblacion_ampliada[:,-1])[::-1]
+    poblacion_ordenada = poblacion_ampliada[indices]
+    poblacion_ordenada_seleccionada = poblacion_ordenada[0:n]
+    poblacion_seleccionada = poblacion_ordenada_seleccionada[:,0:c]
+    for i in range(r):
+        for j in range(c):
+            if (i<n):
+                next_gen[i,j] = poblacion_seleccionada[i,j]
+            else:
+                next_gen[i,j] = np.random.randint(0,2)
+    
+    return next_gen
+
+#************************************************Implementar el algoritmo genetico completo**************************************************** 
+print('\nALGORITMO GENETICO (REPRESENTACION BINARIA)')
+
+#***********************Parametros de arranque********************************
+n=100
+l=8
+generaciones = 100
+ps = 0.6    #porcentaje de seleccion
+pc = n/2    #porcentaje de cruza
+pm = 0.05   #porcentaje de mutacion
+Imin = 0
+Imax = 2
+
+#********************************ALGORITMO GENETICO****************************
+P = poblar(n,l) #Se crea poblacion aleatoria
+mejor_decimal_per_gen = []
+for i in range(generaciones):
+    mejor_aptitud , evaluada_decimal = decodificar(P,Imin,Imax)
+    mejor_decimal_per_gen.append(evaluada_decimal[0])
+    C = cruza(P,pc) #Cruzar la poblacion y guardarla en una matriz
+    M = mutacion(C,pm) #Mutar y guardarla en una matriz
+    next_gen = hacer_ranking(M,ps,Imin,Imax)
+    P = next_gen
+    
+
+#**********************************RESULTADOS***********************************
+P_mejor_decimal,decimal = decodificar(P,Imin,Imax)
+x_mejor = decimal[0]
+print(f'El mejor individuo es: {P_mejor_decimal[0]} con los genes de {P[0,:]}')
+print(f'Con un valor decimal de {x_mejor}\n')
+
+#Graficar la funcion
+x = np.linspace(0,2,100)
+y=f(x)
+y_mejores = []
+for i in mejor_decimal_per_gen:
+    y_mejores.append(f(i))
+plt.plot(x,y)
+plt.scatter(x,y_mejores,color='red')
+plt.grid(True)
+plt.show()
